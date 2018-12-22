31d3c9e922230be7bf0fbcad0af56cfc5c9b83aa
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-22 16:17:31 +0100

    Fetching hits

--- reactfetch/src/App.js
+++ reactfetch/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-const API = '';
+const API = 'https://hn.algolia.com/api/v1/search?query=';
 const DEFAULT_QUERY = 'redux';
 
 
@@ -17,22 +17,24 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(API +DEFAULT_QUERY)
+    fetch(API + DEFAULT_QUERY)
     .then(response => response.json())
-    .then(data => this.setState({ data: data.hits}));
+    .then(data => this.setState({ hits: data.hits}));
   }
   
   render() {
     const { hits } = this.state;
   
     return (
-      //map through data
       <ul>
-        
+        {hits.map(hit =>
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>  
+        )}
       </ul>
     );
   }
 }
-
 
 export default App;
